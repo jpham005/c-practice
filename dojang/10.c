9474b82beb2c729edd01db4bacabb9d60159bf7a
jpham005 <jpham005@naver.com>
2021-07-11 11:42:16 +0900

    도장 10~11

---
+++ dojang/10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main()
+{
+    const long double con1 = -2.225074e-308l;
+    const int con2 = 0x1285;
+    const long long con3 = 9223372036854775807LL;
+ 
+    printf("%Le 0x%x %lld\n", con1, con2, con3);
+ 
+    return 0;
+}
---
+++ dojang/11.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int num1;
+    float num2;
+    char num3;
+
+    scanf("%d %f %c", &num1, &num2, &num3);
+    printf("%d\n%f\n%c", num1, num2, num3);
+}
