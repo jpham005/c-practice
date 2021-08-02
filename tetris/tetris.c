f79645b4f58747c2ca0866c716708beb2bbbf3f9
jpham005 <jpham005@naver.com>
2021-08-02 22:47:01 +0900

    ttris

--- tetris/tetris.c
+++ tetris/tetris.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "shape.h"
 
-int board[22][17];
+int board[22][17] = {0,};
 int cposX, cposY, x, y, z;
 
 void render() {
@@ -58,6 +58,26 @@ void moveLeft() {
     cposX -= 1;
 }
 
+void moveDown() {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (board[cposY + i][cposX + j] == 1 && board[cposY + 1 + i][cposX + j] >= 2)
+                return;
+        }
+
+              
+    }
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (board[cposY + 3 - i][cposX + j] == 1) {
+                board[cposY + 4 - i][cposX + j] = 1;
+                board[cposY + 3 - i][cposX + j] = 0;
+            }
+        }  
+    }
+}
+
 void rotate(int a, int b) {
     if (a == 1) {
         if (b == 3)
@@ -71,7 +91,46 @@ void rotate(int a, int b) {
     
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            board[cposY + 3 - i][cposX + j] = tetrimino_shapes[7][b][(i * 4)+ j];
+            board[cposY + i][cposX + j] = tetrimino_shapes[1][b][(i * 4)+ j];
+        }
+    }
+}
+
+void finish() {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (board[cposY + i][cposX + j] == 1) {
+                board[cposY + i][cposX + j] = 2;
+            }
+        }
+    }    
+}
+
+void complete() {
+    int arr[20] = {0,};
+
+    for (int i = 1; i < 21; i++) {
+        for (int j = 1; j < 11; j++) {
+            if (board[i][j] != 2)
+                break;
+            
+            if (j == 10){
+                for (int k = 1; k < 11; k++) {
+                    board[i][k] = 0;
+                }
+                
+                arr[i] = i;
+            }
+        }
+    }
+
+    for (int i = 2; i < 21; i++) {
+        if (i == arr[i]) {
+            for (int j = i; j > 1; j--) {
+                for (int k = 1; k < 11; k++) {
+                    board[j][k] = board[j - 1][k];
+                }
+            }
         }
     }
 }
@@ -95,13 +154,67 @@ void print() {
     }
 }
 
+int kbhit() {
+    int ch = getchar();
+
+    if(ch != EOF) {
+        ungetc(ch, stdin);
+        return 1;
+    }
+
+    return 0;
+}
+
+void input() {
+    while(1) {       
+        if (kbhit() == 0)
+            break;
+
+        int ch = getchar();
+        switch (ch) {
+            case 27:
+                if (getchar() == 91) {
+                    int b = getchar();
+                    
+                    if (b == 66) { // d
+                        moveDown();
+                        print();
+                    } else if (b == 67) { // r
+                        moveRight();
+                        print();
+                    } else if (b == 68) { // left
+                        moveLeft();
+                        print();
+                    }
+                }
+                break;
+                
+            case 122: // z
+                rotate(-1, 0);
+                print();
+                break;
+        
+            case 99: // c
+                rotate(1, 0);
+                print();
+                break;
+            
+            case 32:  //space
+                finish();
+                print();
+                break;
+        }        
+    }
+}
+
 int main() {
     render();
-    board[10][5] = 1;
-    board[10][6] = 1;
-    board[11][5] = 1;
-    board[11][6] = 1;
-    cposX = 4, cposY = 9;
+    board[1][5] = 1;
+    board[1][6] = 1;
+    board[1][7] = 1;
+    board[1][8] = 1;
+    cposX = 5; 
+    cposY = 0;
+    moveDown();
     print();
-
 }
